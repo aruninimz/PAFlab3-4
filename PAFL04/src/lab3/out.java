ed34f2902b5f6de84e077a44147d1309292af235
aruninimz <68140771+aruninimz@users.noreply.github.com>
2021-03-06 23:42:51 +0530

    First commit

---
+++ PAFL04/src/lab3/out.java
@@ -0,0 +1,10 @@
+package lab3;
+
+public class out extends test {
+	public static void main(String[] args) {
+		//test ob1 = new test();
+		//ob1.connect();
+		//ob1.insertItem("00a","xx" , "250", "aa");
+	
+	}
+}
